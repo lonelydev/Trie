2aa749efae3e6667dc8ceea7f15ff75a0b15aa05
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2017-12-31 21:29:49 +0000

    Add project files.

---
+++ Trie/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trie
+{
+    internal class TrieNode
+    {
+        /*
+         * character to store char at current node
+         * dictionary to store children with child char as key
+         * is word boolean 
+         * 
+         */
+        internal char Character { get; private set; }
+        IDictionary<char, TrieNode> children;
+
+    }
+}
